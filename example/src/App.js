d32449569621ef2d42e8bae7fb17c51a79315629
Carlos Herrera <carlos.herreracaceres@cencosud.cl>
2020-05-22 16:36:13 -0400

    DRAFT example

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'material-form-redux'
+import 'material-form-redux/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
